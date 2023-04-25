d82d76010f14c50bc8de51488d5a7b4aa2dd811a
David Emrich <demrich@me.com>
2023-04-24 23:34:56 -0700

    feat(rpn): CLI setup

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import { createInterface } from "readline";
+import { evaluateRPN } from "./utils/util";
+
+process.stdin.setRawMode(true); // Enable "raw" mode to receive individual keypress events
+process.stdin.setEncoding("utf8"); // Set encoding to UTF-8 to receive characters instead of buffers
+
+const rl = createInterface({ input: process.stdin, output: process.stdout });
+
+console.clear();
+
+rl.prompt();
+
+rl.on('line', (line) => {
+    console.log(evaluateRPN(line));
+    rl.prompt();
+});
+
+process.stdin.on("keypress", (key) => {
+  if (key === "q") {
+    process.exit();
+  }
+});
