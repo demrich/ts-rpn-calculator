a7ccb46dc6c06d84994775173d0abd243282c24c
David Emrich <demrich@me.com>
2023-04-25 10:00:57 -0700

    feat(rpn): update for errors export SYNTH_INSTALL_PATH="$HOME/bin"

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,4 @@
 export type Operator = {
     symbol: string,
     action: (a: number, b:number) => number,
-}
+};
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,54 +1,70 @@
-import { Operator } from "./types";
+import { Operator } from './types';
 
 const stack: number[] = [];
 
 const operators: Operator[] = [
-    {
-      symbol: '+',
-      action: (a: number, b: number) => a + b,
-    },
-    {
-      symbol: '-',
-      action: (a: number, b: number) => a - b,
-    },
-    {
-      symbol: '*',
-      action: (a: number, b: number) => a * b,
-    },
-    {
-      symbol: '/',
-      action: (a: number, b: number) => a / b,
-    },
-  ];
+  {
+    symbol: '+',
+    action: (a: number, b: number) => a + b,
+  },
+  {
+    symbol: '-',
+    action: (a: number, b: number) => a - b,
+  },
+  {
+    symbol: '*',
+    action: (a: number, b: number) => a * b,
+  },
+  {
+    symbol: '/',
+    action: (a: number, b: number) => a / b,
+  },
+];
 
 const isOperator = (token: string): boolean => {
-    return operators.some((op) => op.symbol === token);
+  return operators.some((op) => op.symbol === token);
 };
 
-const performOperation = (operator: Operator): void  => {
-    const [num1, num2] = stack.splice(-2);
-    const result = operator.action(num1, num2);
-    stack.push(result);
-  };
+const performOperation = (
+  num1: number,
+  num2: number,
+  operator: Operator
+): number | string => {
+  if (operator?.symbol === '/' && num2 === 0) {
+    return 'Cannot divide by 0';
+  }
+  const result = operator.action(num1, num2);
+  return result;
+};
 
-export function evaluateRPN(expression: string): number {  
-    const tokens: string[] = expression.split(' ');
-    for (const token of tokens) {
-      if (isOperator(token)) {
-          const operator = operators.find(o => o.symbol === token);
-          operator && performOperation(operator);
-      } else {
-          const convertedToken = parseFloat(token);
-          if(isNaN(convertedToken)) {
-            throw `Invalid operation: "${token}"`;
-          }
-          stack.push(convertedToken);
+export function evaluateRPN(expression: string): number | string {
+  const tokens: string[] = expression.split(' ');
+  for (const token of tokens) {
+    if (isOperator(token)) {
+      const [num1, num2] = stack.splice(-2);
+      if (isNaN(num1) || isNaN(num2)) {
+		stack.length = 0;
+        return 'Invalid inputs';
       }
+      const operator = operators.find((o) => o.symbol === token);
+      const result = performOperation(num1, num2, operator!);
+      if (typeof result === 'string') {
+		stack.length = 0;
+        return result;
+      }
+      stack.push(result);
+    } else {
+      const convertedToken = parseFloat(token);
+      if (isNaN(convertedToken)) {
+		stack.length = 0;
+        return `Invalid operation: "${token}"`;
+      }
+      stack.push(convertedToken);
     }
-    if (stack.length > 1) {
-      const last = stack.slice(-1)[0];
-      return last;
-    }
-    return stack[0];
+  }
+  if (stack.length > 1) {
+    const lastItem = stack.slice(-1)[0];
+    return lastItem;
+  }
+  return stack[0];
 }
-  
