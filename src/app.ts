90021d05c9e4c67549c8917565bccbd8263d0da1
David Emrich <demrich@me.com>
2023-04-25 10:01:21 -0700

    feat(cli): update to quit on q

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import { createInterface } from "readline";
-import { evaluateRPN } from "./utils/util";
+import { createInterface } from 'readline';
+import { evaluateRPN } from './utils/util';
 
 process.stdin.setRawMode(true); // Enable "raw" mode to receive individual keypress events
-process.stdin.setEncoding("utf8"); // Set encoding to UTF-8 to receive characters instead of buffers
+process.stdin.setEncoding('utf8'); // Set encoding to UTF-8 to receive characters instead of buffers
 
 const rl = createInterface({ input: process.stdin, output: process.stdout });
 
@@ -10,13 +10,13 @@ console.clear();
 
 rl.prompt();
 
-rl.on('line', (line) => {
-    console.log(evaluateRPN(line));
-    rl.prompt();
+rl.on('line', (line: string) => {
+  console.log(evaluateRPN(line));
+  rl.prompt();
 });
 
-process.stdin.on("keypress", (key) => {
-  if (key === "q") {
+process.stdin.on('keypress', (key: string) => {
+  if (key === 'q') {
     process.exit();
   }
 });
